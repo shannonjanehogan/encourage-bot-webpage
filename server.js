aa3b86100c3bbde545951f982bd33d9b32c2ffba
sjhogan2 <shannon.jane.hogan@gmail.com>
2016-11-13 11:12:28 -0800

    Render correct file

--- server.js
+++ server.js
@@ -6,16 +6,15 @@ app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 3000));
 
 //For avoiding Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
+app.get('/', function(req, res) {
+    res.render("encouragesplash");
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-app.get("/", (req, res) => {
-  res.render("encouragesplash");
-});
+// app.get("/", (req, res) => {
+//   res.render("encouragesplash");
+// });
 
 app.use(express.static(__dirname + '/styles'));
 
